0680a4008e2d03715d4859a64ba998bb4025a2f9
emdelaville <123672938+emdelaville@users.noreply.github.com>
2023-02-11 13:19:36 -0500

    Update ECE_2312_Program2.m

--- ECE_2312_Program2.m
+++ ECE_2312_Program2.m
@@ -1,9 +1,24 @@
 % Project 2
+
+Fs = 44000;
+f = 5000;  %For first part
+%f = 8000; % For second part
+w0 = 2*pi*f;
+n = 0:(1/Fs):duration;
+amp = 5;
+
+%y = amp*sin(w0*n);
+y = amp * chirp(n, 0, duration, 8000);
+
+% file = 'team[6]-stereosoundfile.wav';
+% [arr, Fs] = audioread(file);
+% audioinfo(file);
+
+% Project 2
 info = audiodevinfo;
 info.input(1)
 info.input(2)
 
-Fs = 44000;
 nBits = 8;
 nChannels = 1;
 duration = 5;
@@ -23,20 +38,30 @@ pause(1);
 disp("1")
 pause(1);
 
-disp("Start Speaking")
+disp("Sound")
 
 record(recorder, duration);
 
-
+sound(y, Fs)
 % Wait 5 seconds
 pause(duration);
 disp("Recording over")
 
 
+%disp(y)
+arr = getaudiodata(recorder, "double");
 
-audioArray = getaudiodata(recorder, "double");
 
-sound(audioArray)
+
+clf
+t = [0: length(y)-1]/ Fs;
+%plot(t, y)
+title("Audio")
+xlabel("Time (sec)")
+ylabel("Magnitude")
+
+
+
 
 window = hamming(512);
 N_overlap = 256;
@@ -52,9 +77,9 @@ ylim([0 8000]);
 xlabel('Time (s)');
 ylabel('Frequency (Hz)');
 
-% filename = 'team[6]-sinetone.wav';
-% audiowrite(filename, arr, Fs);
-% audioinfo(filename)
+filename = 'team[6]-chirp.wav';
+audiowrite(filename, arr, Fs);
+audioinfo(filename)
 
 
 
